19bcf7f1099658db1a2f294361179bfe6d45b5c4
Jake Crawford <jake.crawford@outlook.com>
2018-03-14 11:29:16 -0600

    Add more functionality, comment out to get build to pass

--- src/main/java/PostingController.java
+++ src/main/java/PostingController.java
@@ -1,53 +1,95 @@
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PostingController {
+//    private PostingDAO postingDAO = new PostingDAO();
+//    private PostingFactory postingFactory = new PostingFactory();
+//    private UserFactory userFactory = new UserFactory();
 
     @RequestMapping(value = "/postings/", method = RequestMethod.POST)
-    public Response newPosting(@RequestParam(value="payload") String payload) {
+    public Response newPosting(@RequestParam(value="request") String request) {
         // create new posting
-        return new Response();
+//        User user = userFactory.createUserByRequest(request);
+//
+//        if (!(user instanceof Volunteer)) {
+//            return Response.getBadPermissionsResponse();
+//        }
+//
+//        Posting posting = postingFactory.createPostingByRequest(request);
+//
+//        if (!((Volunteer) user.addPosting(posting))) {
+//            return Response.getServerErrorResponse();
+//        }
+
+        return Response.getSuccessResponse();
     }
 
     @RequestMapping(value = "/postings/", method = RequestMethod.GET)
-    public List<Posting> getAllPostings(@RequestParam(value="payload") String payload) {
+    public List<Posting> getAllPostings(@RequestParam(value="request") String request) {
         // get all users
-        return new ArrayList<Posting>();
+//        User user = userFactory.createUserByRequest(request);
+//        return user.getAllPostings();
+        return new ArrayList<>(); // TEMP
     }
 
     @RequestMapping(value = "/postings/{id}", method = RequestMethod.GET)
-    public Posting getPosting(@RequestParam(value="payload") String payload) {
+    public Posting getPosting(@PathVariable int id, @RequestParam(value="request") String request) {
         // get specific posting
-        return new Posting();
+//        User user = userFactory.createUserByRequest(request);
+//        return user.getPosting(id);
+        return new Posting(); // TEMP
     }
 
-    @RequestMapping(value = "/postings/{id}", method = RequestMethod.PUT)
-    public Response updatePosting(@RequestParam(value="payload") String payload) {
-        // update specific posting
-        return new Response();
-    }
-
-    @RequestMapping(value = "/postings/{id}", method = RequestMethod.PUT)
-    public Response deletePosting(@RequestParam(value="payload") String payload) {
+    @RequestMapping(value = "/postings/{id}", method = RequestMethod.DELETE)
+    public Response deletePosting(@RequestParam(value="request") String request) {
         // delete specific posting
-        return new Response();
+//        User user = userFactory.createUserByRequest(request);
+//
+//        if (!(user instanceof Admin)) {
+//            return Response.getBadPermissionsResponse();
+//        }
+//
+//        Admin admin = (Admin)user;
+//
+//        Posting posting = postingFactory.createPostingByRequest(request);
+//
+//        if (!(admin.removePosting(posting))) {
+//            Response.getServerErrorResponse();
+//        }
+        return Response.getSuccessResponse();
     }
 
-    @RequestMapping(value = "/report/{id}", method = RequestMethod.PUT)
-    public Response reportPosting(@RequestParam(value="payload") String payload) {
+    @RequestMapping(value = "/flag/{id}", method = RequestMethod.PUT)
+    public Response flagPosting(@PathVariable int id, @RequestParam(value="request") String request) {
         // report a specific posting
-        return new Response();
+//        User user = userFactory.createUserByRequest(request);
+//        Posting posting = postingFactory.createPostingById(id);
+//
+//        if (!user.flagPosting(posting)) {
+//            return Response.getServerErrorResponse();
+//        }
+        return Response.getSuccessResponse();
     }
 
     @RequestMapping(value = "/unreport/{id}", method = RequestMethod.PUT)
-    public Response unreportPosting(@RequestParam(value="payload") String payload) {
+    public Response unreportPosting(@PathVariable int id, @RequestParam(value="request") String request) {
         // unreport a specific posting
-        return new Response();
+//        User user = userFactory.createUserByRequest(request);
+//
+//        if (!(user instanceof Admin)) {
+//            return Response.getBadPermissionsResponse();
+//        }
+//
+//        Admin admin = (Admin)user;
+//
+//        Posting posting = postingFactory.createPostingById(id);
+//
+//        if (!admin.allowPosting(posting)) {
+//            return Response.getServerErrorResponse();
+//        }
+        return Response.getSuccessResponse();
     }
 }
