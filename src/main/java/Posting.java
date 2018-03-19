2397ced335720390e36be89015fbdc9027c7315e
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-03-19 14:04:52 -0600

    Forgot to add files last commit

---
+++ src/main/java/Posting.java
@@ -0,0 +1,70 @@
+public class Posting {
+
+    private int id;
+    private String title;
+    private String associatedUsername;
+    private boolean accepted;
+    private boolean completed;
+    private String location;
+
+    public boolean isValid() {
+
+    }
+
+    public int getId() {
+
+    }
+
+    public String getTitle() {
+
+    }
+
+    public String getDescription() {
+
+    }
+
+    public String getAssociatedUsername() {
+
+    }
+
+    public boolean isAccepted() {
+
+    }
+
+    public boolean isCompleted() {
+
+    }
+
+    public String getLocation() {
+
+    }
+
+    public void setId(int id) {
+
+    }
+
+    public void setTitle(String title) {
+
+    }
+
+    public void setDescription(String description) {
+
+    }
+
+    public void setAssociatedUsername(String username) {
+
+    }
+
+    public void setAccepted(boolean accepted) {
+
+    }
+
+    public void setCompleted(boolean completed) {
+
+    }
+
+    public void setLocation(String location) {
+
+    }
+
+}
---
+++ src/main/java/PostingFactory.java
@@ -0,0 +1,13 @@
+public class PostingFactory
+{
+    private int numInstancesMade;
+
+    public Posting createPostingByRequest(String request) {
+        return new Posting;
+    }
+
+    public Posting createPostingById(int id) {
+        return new Posting;
+    }
+
+}
