28e0b6634207546a83d2f0ad6a892c89bb7e4a11
Ryan Turner <ryan.turner@colorado.edu>
2018-03-19 21:13:33 -0600

    User, Admin, Volunteer Classes (#14)
    * User, Admin, Volunteer Classes
    
    * Removed File

---
+++ src/main/java/Admin.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Admin extends User{
+    private int adminId;
+    public Admin(){
+        this.username = "";
+        this.password = "";
+        adminId = -1;
+    }
+    public Admin(String user, String pass, int id){
+        this.username = user;
+        this.password = pass;
+        adminId = id;
+    }
+    public boolean removePosting(Posting post){
+        MysqlDAO mysql = new MysqlUserDAO();
+        List<HashMap<String, Object>> list;
+        String query = "DELETE FROM postings WHERE id = " + post.getId() + ";";
+        list = mysql.getQuery(query);
+        return true;
+    }
+    public boolean allowPosting(Posting post){
+        MysqlDAO mysql = new MysqlUserDAO();
+        List<HashMap<String, Object>> list;
+        String query = "UPDATE postings SET flagged = '0' WHERE id = " + post.getId() + ";";
+        list = mysql.getQuery(query);
+        return true;
+    }
+    public boolean removeUser(User user){
+        MysqlDAO mysql = new MysqlUserDAO();
+        List<HashMap<String, Object>> list;
+        String query = "DELETE FROM users WHERE username = " + post.getUsername() + ";";
+        list = mysql.getQuery(query);
+        return true;
+    }
+    public int getAdminId(){
+        return adminId;
+    }
+    public void setAdminId(int id){
+        adminId = id;
+    }
+}
