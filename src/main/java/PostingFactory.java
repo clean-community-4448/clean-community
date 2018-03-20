9f98bb80b272c533150a01e56eb9c0041062f457
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-03-20 12:39:55 -0600

    Posting and PostingFactory

--- src/main/java/PostingFactory.java
+++ src/main/java/PostingFactory.java
@@ -2,12 +2,39 @@ public class PostingFactory
 {
     private int numInstancesMade;
 
+    PostingFactory() {
+
+        // Query database to get number of postings
+        this.numInstancesMade = 0;
+
+    }
+
     public Posting createPostingByRequest(String request) {
-        return new Posting;
+
+        numInstancesMade++;
+        Posting posting = new Posting(request, numInstancesMade);
+
+        return posting;
     }
 
     public Posting createPostingById(int id) {
-        return new Posting;
+
+        Posting posting = new Posting(id);
+        numInstancesMade++;
+
+        return new Posting(id);
+    }
+
+    // Why is this private?
+    private Posting getPostingFromDatabase(int id) {
+
+        // Querying from database will be next milestone
+
+        return null;
     }
 
 }
+
+
+
+
