3b9dd9ced0878ba6f3aa6f56eb3364b293507f2e
Nerby <t2nerb@gmail.com>
2018-03-25 20:57:42 -0600

    Added UserFactory and utilized where applicable; Added test because I'm bored

---
+++ src/test/java/UserFactoryTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+public class UserFactoryTest {
+
+	private final String username = "johndoe";
+	private final String password = "password";
+	private final String fname = "john";
+	private final String lname = "doe";
+	private final int id = 99;
+	private final Boolean is_admin = true;
+
+
+	@Test
+	public void createUserByRequest() {
+	}
+
+	@Test
+	public void createUser() {
+		User admin = UserFactory.createUser(username, password, fname, lname, id, true);
+		User volunteer = UserFactory.createUser(username, password, fname, lname, id, false);
+
+		System.out.println(admin instanceof Admin);
+		assertThat("admin should be instance of Admin", admin, instanceOf(Admin.class));
+		assertThat("volunteer should be instance of volunteer", volunteer, instanceOf(Admin.class));
+
+	}
+}
