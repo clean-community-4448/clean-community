041bddf759777194a151f1d7b801211630e072b6
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-04-08 17:04:10 -0600

    Empty Vue App

--- frontend/clean-community/src/App.vue
+++ frontend/clean-community/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg="Clean Community"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
