9b33d042a8ce27c609420bdaf607694ad4d79d7b
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-04-20 16:02:05 -0600

    Add leafly map centered to Boulder

--- frontend/clean-community/src/components/Main.vue
+++ frontend/clean-community/src/components/Main.vue
@@ -1,20 +1,67 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div id="Main">
+    <Map></Map>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Map from './Map.vue'
+
 export default {
   name: 'Main',
+
+  components: {
+    Map
+  },
+
   props: {
     msg: String
+  },
+
+  mounted() {
+    axios.get('/postings/')
+      .then(res => {
+        console.log(res);
+        this.test = res;
+      })
+      .catch(err => {
+        this.test = err;
+      });
+  },
+
+  data() {
+    return {
+      test: "blah"
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+
+.leaflet-fake-icon-image-2x {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
+}
+.leaflet-fake-icon-shadow {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
+}
+@import "../../node_modules/leaflet/dist/leaflet.css";
+#Main {
+  position: absolute;
+  overflow-x: auto;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+body {
+  margin: 0px;
+}
+/*
 h3 {
   margin: 40px 0 0;
 }
@@ -29,4 +76,5 @@ li {
 a {
   color: #42b983;
 }
+*/
 </style>
---
+++ frontend/clean-community/src/components/Map.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="Map">
+    <div style="height: 10%; overflow: auto;">
+      <h3>Simple map</h3>
+      Marker is placed at {{ marker.lat }}, {{ marker.lng }}
+    </div>
+    <l-map style="height: 90%; width: 100%" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+export default {
+  name: 'Map',
+
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
+
+  data () {
+    return {
+      zoom:13,
+      center: [40.0150, -105.2705],
+      marker: [40.0150, -105.2705],
+    }
+  }
+
+}
+</script>
+
+<style>
+#Map {
+  width: 100%;
+  height: 100%;
+}
+</style>
