9831a6d60b0fc5b29c6dbcf2762c09430e25a73f
Ryan Rouleau <ryan.c.rouleau@live.com>
2018-04-22 17:43:40 -0600

    Completed frontend for two use cases

--- frontend/clean-community/src/components/Main.vue
+++ frontend/clean-community/src/components/Main.vue
@@ -1,54 +1,75 @@
 <template>
   <div id="Main">
-    <Map></Map>
+
+    <transition name="fade">
+
+      <div id="admin-check" v-if="!adminChoiceSelected">
+        <h1>Are you an Admin or Volunteer?</h1>
+        <button @click="setAdmin">I'm an Admin!</button>
+        <button @click="setVolunteer">I'm a Volunteer!</button>
+      </div>
+
+    </transition>
+
+    <Map :isAdmin="isAdmin"></Map>
+    <AdminInfoCard v-if="isAdmin"></AdminInfoCard>
+    <VolunteerInfoCard v-if="!isAdmin"></VolunteerInfoCard>
+    <NewPostingForm v-if="!isAdmin"></NewPostingForm>
+
+    <div id="toggle-admin-chooser">
+      <button @click="adminChoiceSelected = false">Switch Account</button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 import Map from './Map.vue'
+import NewPostingForm from './NewPostingForm.vue'
+import AdminInfoCard from './AdminInfoCard.vue'
+import VolunteerInfoCard from './VolunteerInfoCard.vue'
+
 
 export default {
   name: 'Main',
 
   components: {
-    Map
+    Map,
+    AdminInfoCard,
+    VolunteerInfoCard,
+    NewPostingForm
   },
 
   props: {
     msg: String
   },
 
-  mounted() {
-    axios.get('/postings/')
-      .then(res => {
-        console.log(res);
-        this.test = res;
-      })
-      .catch(err => {
-        this.test = err;
-      });
+  methods: {
+
+    setAdmin () {
+      this.isAdmin = true;
+      this.adminChoiceSelected = true;
+    },
+
+    setVolunteer () {
+      this.isAdmin = false;
+      this.adminChoiceSelected = true;
+    }
+
   },
 
-  data() {
+  data () {
     return {
-      test: "blah"
+      isAdmin: false,
+      adminChoiceSelected: false
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
-.leaflet-fake-icon-image-2x {
-  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
-}
-.leaflet-fake-icon-shadow {
-  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
-}
-@import "../../node_modules/leaflet/dist/leaflet.css";
 #Main {
+  z-index: 0;
   position: absolute;
   overflow-x: auto;
   width: 100%;
@@ -61,20 +82,30 @@ export default {
 body {
   margin: 0px;
 }
-/*
-h3 {
-  margin: 40px 0 0;
+#admin-check {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: white;
+  top: 0;
+  left: 0;
+  z-index: 10000000000000000;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#admin-check button {
+  font-size: 18px;
+  margin: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+#toggle-admin-chooser {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 10px 10px;
+  z-index: 100000000;
 }
-a {
-  color: #42b983;
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .7s;
 }
-*/
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
--- frontend/clean-community/src/components/Map.vue
+++ frontend/clean-community/src/components/Map.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="Map">
-    <div style="height: 10%; overflow: auto;">
-      <h3>Simple map</h3>
-      Marker is placed at {{ marker.lat }}, {{ marker.lng }}
-    </div>
-    <l-map style="height: 90%; width: 100%" :zoom="zoom" :center="center">
+    <l-map style="height: 100%; width: 100%" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <marker-popup :isAdmin="isAdmin" :posting="posting" v-for="posting in validPostings" :key="posting.id"></marker-popup>
     </l-map>
   </div>
 </template>
 
 <script>
+import MarkerPopup from './MarkerPopup.vue';
+import Bus from './bus.js';
+
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import axios from 'axios';
 
 export default {
   name: 'Map',
 
+  props: ['isAdmin'],
+
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    MarkerPopup
+  },
+
+  mounted() {
+    Bus.$on('refresh-markers', this.addMarkers);
+    this.addMarkers();
+  },
+
+  methods: {
+
+    addMarkers () {
+      axios.get('/postings/')
+        .then(res => {
+          this.postings = res.data;
+          console.log(this.postings);
+        })
+        .catch(err => {
+          alert(err);
+        })
+    }
+
+  },
+
+  computed: {
+
+    validPostings () {
+      return this.postings.filter(posting => {
+        // If admin, display all posts, even reported ones
+        if (this.isAdmin) return true;
+        // If volunteer, hide reported posts until admin unreports
+        return !posting.flagged && !this.isAdmin;
+      })
+    }
+
   },
 
   data () {
     return {
-      zoom:13,
-      center: [40.0150, -105.2705],
-      marker: [40.0150, -105.2705],
+      zoom:14,
+      center: [40.0150, -105.2705], // default to boulder lat/lang
+      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      postings: []
     }
   }
 
@@ -34,6 +73,13 @@ export default {
 </script>
 
 <style>
+.leaflet-fake-icon-image-2x !important {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
+}
+.leaflet-fake-icon-shadow !important {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
+}
+@import "../../node_modules/leaflet/dist/leaflet.css";
 #Map {
   width: 100%;
   height: 100%;
